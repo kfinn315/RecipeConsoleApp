78c6aa5552ff63930d30486d106cd469f41bbdee
kfinn315 <kfinn315@gmail.com>
2025-07-30 10:52:58 -0400

    Move into separate Projects

--- Project.CLI/Program.cs
+++ Project.CLI/Program.cs
@@ -3,10 +3,10 @@ using RecipeConsoleApp.Core.Entities;
 using RecipeConsoleApp.Infrastructure.Database;
 using RecipeConsoleApp.Infrastructure.Repositories;
 
-var categoryDatastore = new JsonDataStorage<List<Category>>(new FileStreamIO("./Infrastructure/Data/categories.json"));
+var categoryDatastore = new JsonDataStorage<List<Category>>(new FileStreamIO("Project.Infrastructure/Data/categories.json"));
 var categoryRepo = new CategoryListRepository(categoryDatastore);
 
-var recipeDatastore = new JsonDataStorage<List<Recipe>>(new FileStreamIO("./Infrastructure/Data/recipes.json"));
+var recipeDatastore = new JsonDataStorage<List<Recipe>>(new FileStreamIO("Project.Infrastructure/Data/recipes.json"));
 var recipeRepo = new RecipeListRepository(recipeDatastore);
 
 ProgramCLI.Run(categoryRepo, recipeRepo);
