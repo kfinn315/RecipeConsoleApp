abaf23fb1d2a2d73a5b927c11b882300b1feaf74
kfinn315 <kfinn315@gmail.com>
2025-08-11 14:12:59 -0400

    Miscellaneous changes

--- Project.SPA/src/Components/Pages/WelcomePage.tsx
+++ Project.SPA/src/Components/Pages/WelcomePage.tsx
@@ -4,7 +4,7 @@ export function WelcomePage() {
 
     return <div>
         <h2>Intro Page</h2>
-        <p>Welcome to the React Console App!</p>
+        <p>Welcome to the Recipe App!</p>
     </div>;
 }
 
---
+++ Project.SPA/src/Types/index.ts
@@ -0,0 +1,4 @@
+import { Category } from './Category';
+import { Recipe } from './Recipe';
+
+export { Category, Recipe }
