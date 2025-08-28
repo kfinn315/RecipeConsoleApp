c4b5953a53ca38e026494c595e419be7f4206361
kfinn315 <kfinn315@gmail.com>
2025-08-28 16:55:21 -0400

    Create msw auto-mocks

--- Project.SPA/mocks/setupTests.ts
+++ Project.SPA/mocks/setupTests.ts
@@ -1,18 +1,5 @@
-// // import { setupServer } from "msw/node"
-// // import { handlers } from "../mocks/handlers";
-// // import { handlers as automockHandlers } from "./mock";
-// import { fromOpenApi } from '@mswjs/source/open-api';
-// import { setupServer } from 'msw/node';
-// // import { handlers_openapi } from '../mocks/handlers_openapi';
-// import apiSpec from './openapi.json'; 
+import { server } from "./node"
 
-// export async function getServer() {
-//     const handlers_openapi = await fromOpenApi(apiSpec);
-
-//     // Start the mock server
-//     const server = setupServer(...handlers_openapi);
-//     return server;
-// }
-// // beforeAll(() => server.listen());
-// // afterEach(() => server.resetHandlers());
-// // afterAll(() => server.close());
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
