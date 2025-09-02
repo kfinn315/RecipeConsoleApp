defa0a43d483842c2acee4c03526b99901d205b2
kfinn315 <kfinn315@gmail.com>
2025-09-02 15:06:02 -0400

    Add logging to BaseClient.ts

--- Project.SPA/src/Client/BaseClient.ts
+++ Project.SPA/src/Client/BaseClient.ts
@@ -4,18 +4,22 @@ export class BaseClient {
     constructor() { }
 
     get<T>(url: string): Promise<T> {
+        console.info('BaseClient get', url);
         return fetch(url, { method: "GET", headers: { 'Content-Type': 'application/json' } })
             .then(response => {
+                console.info('fetch response', response)
                 if (response.ok) {
                     return response.json();
                 }
                 throw new Error(response.statusText);
             }).catch((reason) => {
+                console.error('from fetch', reason)
                 throw reason;
             });
     }
 
     add<T extends { id: number }>(url: string, item: OptionalID<T>): Promise<T> {
+        console.info('BaseClient add', url, item);
         return fetch(url, {
             method: "POST",
             body: JSON.stringify(item),
@@ -23,16 +27,19 @@ export class BaseClient {
                 'Content-Type': 'application/json'
             },
         }).then(response => {
+            console.info('fetch response', response)
             if (response.ok) {
                 return response.json();
             }
             throw new Error(response.statusText);
         }).catch((reason) => {
+            console.error('from fetch', reason)
             throw reason;
         });
     }
 
     update<T>(url: string, item: T): Promise<T> {
+        console.info('BaseClient update', url, item);
         return fetch(url, {
             method: "PUT",
             body: JSON.stringify(item),
@@ -40,11 +47,13 @@ export class BaseClient {
                 'Content-Type': 'application/json'
             },
         }).then(response => {
+            console.info('fetch response', response)
             if (response.ok) {
                 return response.json();
             }
             throw new Error(response.statusText);
         }).catch((reason) => {
+            console.error('from fetch', reason)
             throw reason;
         });
     }
